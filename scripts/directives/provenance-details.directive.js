e9d17922d329edc8528f859ab00b6a54e2f2f579
Thomas 'Panda' Attwood | TickleThePanda <thomas.attwood@stfc.ac.uk>
2016-11-24 14:37:27 +0000

    #3 using tabular layout

--- scripts/directives/provenance-details.directive.js
+++ scripts/directives/provenance-details.directive.js
@@ -2,7 +2,7 @@
 
     var app = angular.module('topcat');
 
-    app.directive('provenanceDetails', function($stateParams) {
+    app.directive('provenanceDetails', function($stateParams, uiGridConstants) {
         return {
             restrict: 'E',
             scope: {
@@ -10,20 +10,25 @@
             },
             templateUrl: $stateParams.pluginUrl + 'views/' + 'provenance-details.html',
             controller: function($scope, $timeout, $state) {
-                var dataQueryTypes = {
+
+                var that = this;
+
+                that.dataQueryTypes = {
                     dataset: {
                         "type": "dataset",
                         "classType": "Dataset",
-                        "view": "datasets"
+                        "view": "datasets",
+                        "include": " include d.type"
                     },
                     datafile: {
                         "type": "datafile",
                         "classType": "Datafile",
-                        "view": "datafiles"
+                        "view": "datafiles",
+                        "include": " include d.datafileFormat"
                     }
                 };
 
-                var ioTypes = {
+                that.ioTypes = {
                     input: {
                         "type": "input",
                         "classType": "Input",
@@ -36,16 +41,56 @@
                     }
                 };
 
-                var that = this;
                 that.input = {
-                    datasets: [],
-                    datafiles: []
+                    gridOptions: {
+                        data: [],
+                        appScopeProvider: this
+                    }
                 }
                 that.output = {
-                    datasets: [],
-                    datafiles: []
+                    gridOptions: {
+                        data: [],
+                        appScopeProvider: this
+                    }
                 }
                 that.arguments = "";
+
+                that.pagingConfig = tc.config().paging;
+
+                function setUpGridOptions(grid){
+
+                    grid.enableHorizontalScrollbar = uiGridConstants.scrollbars.NEVER;
+                    grid.enableRowSelection =  false;
+                    grid.enableRowHeaderSelection =  false;
+                    grid.gridMenuShowHideColumns =  false;
+                    grid.enablePaginationControls = false;
+                    grid.pageSize =  !that.isScroll ? that.pagingConfig.paginationNumberOfRows : null;
+                    grid.paginationPageSizes =  that.pagingConfig.paginationPageSizes;
+                    grid.paginationNumberOfRows =  that.pagingConfig.paginationNumberOfRows;
+
+                    grid.columnDefs = [
+                        {
+                            "field": "name",
+                            "displayName": "MY_JOBS.JOB_DETAILS.MODAL.TABS.PROVENANCE.COLUMNS.NAME"
+                        },
+                        {
+                            "field": "dt",
+                            "displayName": "MY_JOBS.JOB_DETAILS.MODAL.TABS.PROVENANCE.COLUMNS.TYPE"
+                        },
+                        {
+                            "field": "getType()",
+                            "displayName": "MY_JOBS.JOB_DETAILS.MODAL.TABS.PROVENANCE.COLUMNS.DATATYPE"
+                        }
+                    ]
+
+                    _.each(grid.columnDefs, function(columnDef){
+                        columnDef.headerCellFilter = 'translate';
+                        columnDef.enableHiding = false;
+                    });
+                }
+
+                setUpGridOptions(that.input.gridOptions);
+                setUpGridOptions(that.output.gridOptions);
 
                 var facility = tc.facility($state.params.facilityName);
                 var icat = facility.icat();
@@ -60,7 +105,8 @@
                             + " where job.id = " + provenanceId + " "
                             + " and job." + ioType.type + "DataCollection = dc "
                             + " and dc.dataCollection" + dataQueryType.classType + "s = dcs "
-                            + " and dcs." + dataQueryType.type + " = d");
+                            + " and dcs." + dataQueryType.type + " = d"
+                            + dataQueryType.include);
                 }
 
                 function createJobQuery(provenanceId) {
@@ -70,13 +116,23 @@
                             + " where job.id = " + provenanceId);
                 }
 
-                Object.keys(dataQueryTypes).forEach(function(dataQueryTypeKey) {
-                    Object.keys(ioTypes).forEach(function(ioTypeKey) {
-                        var dataQueryType = dataQueryTypes[dataQueryTypeKey];
-                        var ioType = ioTypes[ioTypeKey];
+                Object.keys(that.dataQueryTypes).forEach(function(dataQueryTypeKey) {
+                    Object.keys(that.ioTypes).forEach(function(ioTypeKey) {
+                        var dataQueryType = that.dataQueryTypes[dataQueryTypeKey];
+                        var ioType = that.ioTypes[ioTypeKey];
                         createDataQuery(dataQueryType, ioType, $scope.provenanceId).then(function(results) {
                             results.forEach(function(dataset) {
-                                that[ioType.view][dataQueryType.view].push(dataset);
+                                dataset.dt = dataQueryType.classType;
+
+                                dataset.getType = function() {
+                                    if (this.dt === that.dataQueryTypes.dataset.classType) {
+                                        return this.type.name;
+                                    } else {
+                                        return this.datafileFormat.name;
+                                    }
+                                }
+
+                                that[ioType.view].gridOptions.data.push(dataset);
                             });
                         });
                     });
